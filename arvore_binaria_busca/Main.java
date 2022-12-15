86dbb0f7b520cebb6d74f17c89e3cdf8868db4d5
Arthur André <75803345+arthur272-debug@users.noreply.github.com>
2022-12-15 13:41:40 -0300

    Add files via upload

---
+++ arvore_binaria_busca/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ArvoreBinariaBusca<Integer> arvoreBinaria = new ArvoreBinariaBusca<>();
+
+        //int [] vetor = {1,2,3,4,5};
+
+       // arvoreBinaria.constroiArvore(vetor);
+
+        arvoreBinaria.inserirValor(1);
+        arvoreBinaria.inserirValor(2);
+        arvoreBinaria.inserirValor(3);
+        arvoreBinaria.inserirValor(4);
+        arvoreBinaria.inserirValor(5);
+        arvoreBinaria.inserirValor(6);
+        arvoreBinaria.inserirValor(7);
+        arvoreBinaria.inserirValor(8);
+
+    }
+}
